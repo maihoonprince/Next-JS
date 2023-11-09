6353ca8f6d4c6d36119ff3e2301bd521c3a2b77d
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-11-09 18:19:37 +0530

    Add files via upload

---
+++ Routing/app/Courses/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const courses = () => {
+  return (
+    <div> courses</div>
+  )
+}
+
+export default courses
---
+++ Routing/app/page.js
@@ -0,0 +1,21 @@
+import Header from '@/Components/Header'
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <div>
+
+      
+      <h1>This is Homepage</h1>
+      
+
+
+      {/* <a href='/About'>About</a>
+      <a href='/Product'>Product</a>
+      <a href='/Courses'>Courses</a> */}
+    </div>
+  )
+}
+
+export default page
