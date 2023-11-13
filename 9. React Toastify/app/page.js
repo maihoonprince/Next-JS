7cbf900c7904c4686a7a0b789a432b952dccfa82
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-11-13 22:51:48 +0530

    Add files via upload
    In this i have studied about how to show flash message with the help of React Toastify. We can also customize our messages...

---
+++ 9. React Toastify/app/page.js
@@ -0,0 +1,29 @@
+"use client"
+
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+const page = () => {
+  const notify = () => {
+    toast.success('🦄 Wow so easy!', {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      });
+  }
+  return (
+    <div>
+      <button onClick={notify}>Login</button>
+      <ToastContainer />
+    </div>
+  )
+}
+
+export default page
